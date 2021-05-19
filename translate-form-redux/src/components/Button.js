3e427d0842e44f77df80c2334b45b04672e71924
anshul1710 <anshul.gandotraa88@gmail.com>
2021-05-19 14:55:13 +0530

    first commit

---
+++ translate-form-redux/src/components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
+class Button extends React.Component{
+   
+    renderButton(color){
+        return(
+            <button className={`ui button ${color}`}>
+                <LanguageContext.Consumer>
+                    {({language})=>language==='english'? 'Submit':'Voorleggen'}
+                </LanguageContext.Consumer>
+                </button>
+        );
+    }
+    render(){
+
+        // const text=this.context==='english'? 'Submit':'Voorleggen';
+        return(
+            <ColorContext.Consumer>
+            {(color)=>this.renderButton(color)}
+            </ColorContext.Consumer>
+        );
+    }
+}
+export default Button;
---
+++ translate-form/src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
+class App extends React.Component{
+        state={language:'english'};
+
+        onLanguageChange= (language) =>{
+            this.setState({language:language});
+        }
+        render(){
+            return(
+                <div className="ui container">
+                   <div>
+                       Select a language:
+                       <i className="flag us" onClick={()=>this.onLanguageChange('english')}></i>
+                       <i className="flag nl" onClick={()=>this.onLanguageChange('dutch')}></i>
+                   </div>
+                   <LanguageContext.Provider value={this.state.language}>
+                       <ColorContext.Provider value='red'>
+                  <UserCreate />
+                  </ColorContext.Provider>
+                  </LanguageContext.Provider>
+                </div>
+            );
+        }
+
+}
+export default App;
